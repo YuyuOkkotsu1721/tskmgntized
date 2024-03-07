dec23fde725def5ae8adfe926925509d262851e8
YuyuOkkotsu1721 <143156825+YuyuOkkotsu1721@users.noreply.github.com>
2024-03-07 15:55:25 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'taskmagnet');
+define('DB_SERVER', 'otmaa16c1i9nwrek.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306');
+define('DB_USERNAME', 'wh6san4fkk2o66qp');
+define('DB_PASSWORD', 'mf43izps0pih74lz');
+define('DB_NAME', 'jpdb11jbga05zt8q');
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
